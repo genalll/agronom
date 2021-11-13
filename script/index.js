0b3ee49edcb8b6c97021342d30fe4baa05eb2c63
genalll <genalll@mail.ru>
2021-11-13 17:03:12 +0500

    Исправил отображение культур по районам.

--- script/index.js
+++ script/index.js
@@ -20,16 +20,17 @@ distrikt2.classList.add("distrikt");
 distrikt3.classList.add("distrikt");
 distrikt4.classList.add("distrikt");
 
-sernursk={description:"температура воздуха от 5.1 до 25.3 градусов цельсия, почвы ,дерново-подзолистые, суглинистые, влажность 66-68, осадки 360-380, РН почвы 6,7"};
-yurirsk={description:"температура воздуха от 5.6 до 25.1 градусов цельсия,почвы дерново-слабо и средне-подзолистыми песчаными почвами, влажность 66-68, осадки 360-380, РН почвы 6,7"};
-yoskarla={description:"температура воздуха от 5.8 до 25.5 градусов цельсия,дерново-подзолистые, суглинистые, влажность 66-68, осадки 360-380, РН почвы 6,7"};
-voljskiy={description:"температура воздуха от 5 до 25.5 градусов цельсия, почвы дерново-подзолистые почвы, песчанные   влажность 66-68, осадки 360-380, РН почвы 6,7"};
+sernursk={description:"температура воздуха от 5.1 до 25.3 градусов цельсия, почвы ,дерново-подзолистые, суглинистые, влажность 66-68, осадки 360-380, РН почвы 6,7",eco:"Высокий экономический потенциал: картофель, морковь, свекла, дыня, арбуз \n Средний экономический потенциал: капуста, огурец \n Низкий экономический потенциал: рис"};
+yurirsk={description:"температура воздуха от 5.6 до 25.1 градусов цельсия,почвы дерново-слабо и средне-подзолистыми песчаными почвами, влажность 66-68, осадки 360-380, РН почвы 6,7",eco:"Высокий экономический потенциал: картофель, морковь, свекла, дыня, арбуз \n Средний экономический потенциал: огурец \n Низкий экономический потенциал: капуста, рис"};
+yoskarla={description:"температура воздуха от 5.8 до 25.5 градусов цельсия,дерново-подзолистые, суглинистые, влажность 66-68, осадки 360-380, РН почвы 6,7",eco:"Высокий экономический потенциал: картофель, морковь, свекла, дыня, арбуз \n Средний экономический потенциал: капуста, огурец \n Низкий экономический потенциал: рис"};
+voljskiy={description:"температура воздуха от 5 до 25.5 градусов цельсия, почвы дерново-подзолистые почвы, песчанные   влажность 66-68, осадки 360-380, РН почвы 6,7",eco:"Высокий экономический потенциал: картофель, морковь, свекла, дыня, арбуз \n Средний экономический потенциал: огурец \n Низкий экономический потенциал: капуста, рис"};
 
 
 class Card {
     constructor(cardObj) {
         this.name = cardObj.name;
         this.description = cardObj.description;
+        this.eco=cardObj.eco;
         
             
     }
@@ -38,11 +39,13 @@ class Card {
         const cardElement = document.createElement('article');
         const cardName = document.createElement('h3');
         const cardDescription = document.createElement('p');
+        const cardeco = document.createElement('p');
         
 
 
 
-        cardElement.classList.add("card");
+        cardElement.classList.add("features__offer-text");
+        cardElement.classList.add("features__offer-text_result");
         
         cardName.classList.add("card__name");
       
@@ -50,12 +53,13 @@ class Card {
 
         cardName.textContent = this.name;
         cardDescription.textContent = this.description;
-        
+        cardeco.textContent = this.eco;
 
         
         
         cardElement.appendChild(cardName);
         cardElement.appendChild(cardDescription);
+        cardElement.appendChild(cardeco);
     
         this.cardElement = cardElement;
         return this.cardElement;
@@ -118,17 +122,21 @@ buttonSelectRegion.onclick = function readContent(event,regionMap){
     let description=""
     if (result.textContent=="Сернурский район"){
          description =  sernursk.description;
+         eco=sernursk.eco;
     }
     if (result.textContent=="Йошкар-Ола"){
         description =  yoskarla.description;
+        eco=yoskarla.eco
    }
    if (result.textContent=="Юринский район"){
     description =  yurirsk.description;
+    eco=yurirsk.eco
 }
 if (result.textContent=="Волжский район"){
     description =  voljskiy.description;
+    eco=voljskiy.eco
 }
-    const card =new Card({name:result.textContent,description:description});
+    const card =new Card({name:result.textContent,description:description,eco:eco});
     const cards = card.cardCreate();
     features.textContent="";
     console.log(cards);
