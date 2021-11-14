2e7694ea1a1e0900bd6b62d0a78232aed37354de
genalll <genalll@mail.ru>
2021-11-14 09:41:34 +0500

    Форматируем код.

--- script/index.js
+++ script/index.js
@@ -3,36 +3,36 @@ const buttonSelectRegion = document.querySelector("#seleckt");
 const buttonResetRegion = document.querySelector("#reset");
 const features = document.querySelector(".features");
 const region = document.querySelector(".region");
-const regionMap="";
+const regionMap = "";
 const distrikt1 = document.createElement("p");
 const distrikt2 = document.createElement("p");
 const distrikt3 = document.createElement("p");
 const distrikt4 = document.createElement("p");
 const result = document.createElement("p");
 result.classList.add("result");
-result.textContent="Я крутой результат вычислений"
-distrikt1.textContent="Йошкар-Ола"
-distrikt2.textContent="Юринский район"
-distrikt3.textContent="Сернурский район"
-distrikt4.textContent="Волжский район"
+result.textContent = "Я крутой результат вычислений"
+distrikt1.textContent = "Йошкар-Ола"
+distrikt2.textContent = "Юринский район"
+distrikt3.textContent = "Сернурский район"
+distrikt4.textContent = "Волжский район"
 distrikt1.classList.add("distrikt");
 distrikt2.classList.add("distrikt");
 distrikt3.classList.add("distrikt");
 distrikt4.classList.add("distrikt");
 
-sernursk={description:"температура воздуха от 5.1 до 25.3 градусов цельсия, почвы ,дерново-подзолистые, суглинистые, влажность 66-68, осадки 360-380, РН почвы 6,7",eco:"Высокий экономический потенциал: картофель, морковь, свекла, дыня, арбуз \n Средний экономический потенциал: капуста, огурец \n Низкий экономический потенциал: рис"};
-yurirsk={description:"температура воздуха от 5.6 до 25.1 градусов цельсия,почвы дерново-слабо и средне-подзолистыми песчаными почвами, влажность 66-68, осадки 360-380, РН почвы 6,7",eco:"Высокий экономический потенциал: картофель, морковь, свекла, дыня, арбуз \n Средний экономический потенциал: огурец \n Низкий экономический потенциал: капуста, рис"};
-yoskarla={description:"температура воздуха от 5.8 до 25.5 градусов цельсия,дерново-подзолистые, суглинистые, влажность 66-68, осадки 360-380, РН почвы 6,7",eco:"Высокий экономический потенциал: картофель, морковь, свекла, дыня, арбуз \n Средний экономический потенциал: капуста, огурец \n Низкий экономический потенциал: рис"};
-voljskiy={description:"температура воздуха от 5 до 25.5 градусов цельсия, почвы дерново-подзолистые почвы, песчанные   влажность 66-68, осадки 360-380, РН почвы 6,7",eco:"Высокий экономический потенциал: картофель, морковь, свекла, дыня, арбуз \n Средний экономический потенциал: огурец \n Низкий экономический потенциал: капуста, рис"};
+sernursk = { description: "температура воздуха от 5.1 до 25.3 градусов цельсия, почвы ,дерново-подзолистые, суглинистые, влажность 66-68, осадки 360-380, РН почвы 6,7", eco: "Высокий экономический потенциал: картофель, морковь, свекла, дыня, арбуз \n Средний экономический потенциал: капуста, огурец \n Низкий экономический потенциал: рис" };
+yurirsk = { description: "температура воздуха от 5.6 до 25.1 градусов цельсия,почвы дерново-слабо и средне-подзолистыми песчаными почвами, влажность 66-68, осадки 360-380, РН почвы 6,7", eco: "Высокий экономический потенциал: картофель, морковь, свекла, дыня, арбуз \n Средний экономический потенциал: огурец \n Низкий экономический потенциал: капуста, рис" };
+yoskarla = { description: "температура воздуха от 5.8 до 25.5 градусов цельсия,дерново-подзолистые, суглинистые, влажность 66-68, осадки 360-380, РН почвы 6,7", eco: "Высокий экономический потенциал: картофель, морковь, свекла, дыня, арбуз \n Средний экономический потенциал: капуста, огурец \n Низкий экономический потенциал: рис" };
+voljskiy = { description: "температура воздуха от 5 до 25.5 градусов цельсия, почвы дерново-подзолистые почвы, песчанные   влажность 66-68, осадки 360-380, РН почвы 6,7", eco: "Высокий экономический потенциал: картофель, морковь, свекла, дыня, арбуз \n Средний экономический потенциал: огурец \n Низкий экономический потенциал: капуста, рис" };
 
 
 class Card {
     constructor(cardObj) {
         this.name = cardObj.name;
         this.description = cardObj.description;
-        this.eco=cardObj.eco;
-        
-            
+        this.eco = cardObj.eco;
+
+
     }
 
     cardCreate() {
@@ -41,32 +41,32 @@ class Card {
         const cardDescription = document.createElement('p');
         const cardeco = document.createElement('p');
         const button = document.createElement('button');
-        
+
 
 
 
         cardElement.classList.add("features__offer-text");
         cardElement.classList.add("features__offer-text_result");
-        
+
         cardName.classList.add("card__name");
         button.classList.add("hero-button");
         button.classList.add("hero-button_margin");
         button.classList.add("hero-button_tocalk");
         button.textContent = "Перейти к глубокому анализу";
-      
+
 
 
         cardName.textContent = this.name;
         cardDescription.textContent = this.description;
         cardeco.textContent = this.eco;
 
-        
-        
+
+
         cardElement.appendChild(cardName);
         cardElement.appendChild(cardDescription);
         cardElement.appendChild(cardeco);
         cardElement.appendChild(button);
-    
+
         this.cardElement = cardElement;
         return this.cardElement;
     }
@@ -81,18 +81,18 @@ class Card {
 
 function closeImagePopup(event) {
     console.log(event.target.id);
-    if (event.target.shape=="poly") {
-        region.textContent='';
-        region.textContent=event.target.id;
-        
-        
+    if (event.target.shape == "poly") {
+        region.textContent = '';
+        region.textContent = event.target.id;
+
+
         if (event.target.id != "Республика Марий Эл") {
-            result.textContent="Для этого региона к сожалению не разработано критериев оценки эфективности сельского хозяйства"    
-            
+            result.textContent = "Для этого региона к сожалению не разработано критериев оценки эфективности сельского хозяйства"
+
         }
 
 
-        if (event.target.id=="Республика Марий Эл") {
+        if (event.target.id == "Республика Марий Эл") {
             region.appendChild(distrikt1);
             region.appendChild(distrikt2);
             region.appendChild(distrikt3);
@@ -100,16 +100,16 @@ function closeImagePopup(event) {
         }
     }
 
-    
+
 }
-    
 
 
-function selectRegion(event){
-    if (event.target.classList.contains('distrikt')){
+
+function selectRegion(event) {
+    if (event.target.classList.contains('distrikt')) {
         event.target.classList.add("distriktred");
-        result.textContent=event.target.textContent
-        console.log(result.textContent+" На данный момент")
+        result.textContent = event.target.textContent
+        console.log(result.textContent + " На данный момент")
         this.removeEventListener('click', selectRegion);
 
     }
@@ -124,37 +124,37 @@ document.addEventListener('click', goTocalk);
 
 
 
-buttonSelectRegion.onclick = function readContent(event,regionMap){
+buttonSelectRegion.onclick = function readContent(event, regionMap) {
     console.log("Я выбрал регион")
-    let description=""
-    if (result.textContent=="Сернурский район"){
-         description =  sernursk.description;
-         eco=sernursk.eco;
+    let description = ""
+    if (result.textContent == "Сернурский район") {
+        description = sernursk.description;
+        eco = sernursk.eco;
     }
-    if (result.textContent=="Йошкар-Ола"){
-        description =  yoskarla.description;
-        eco=yoskarla.eco
-   }
-   if (result.textContent=="Юринский район"){
-    description =  yurirsk.description;
-    eco=yurirsk.eco
-}
-if (result.textContent=="Волжский район"){
-    description =  voljskiy.description;
-    eco=voljskiy.eco
-}
-    const card =new Card({name:result.textContent,description:description,eco:eco});
+    if (result.textContent == "Йошкар-Ола") {
+        description = yoskarla.description;
+        eco = yoskarla.eco
+    }
+    if (result.textContent == "Юринский район") {
+        description = yurirsk.description;
+        eco = yurirsk.eco
+    }
+    if (result.textContent == "Волжский район") {
+        description = voljskiy.description;
+        eco = voljskiy.eco
+    }
+    const card = new Card({ name: result.textContent, description: description, eco: eco });
     const cards = card.cardCreate();
-    features.textContent="";
+    features.textContent = "";
     console.log(cards);
     features.appendChild(cards);
-    
+
 }
 
-buttonResetRegion.onclick = function resetContent(event,regionMap){
+buttonResetRegion.onclick = function resetContent(event, regionMap) {
     console.log("Я сбросил регион")
     document.location.href = "index.html";
-    
+
 }
 
 
@@ -164,7 +164,8 @@ buttonResetRegion.onclick = function resetContent(event,regionMap){
 
 
 
-function goTocalk(event){
-    if (event.target.classList.contains('hero-button_tocalk')){
-    document.location.href = "calck.html";}
+function goTocalk(event) {
+    if (event.target.classList.contains('hero-button_tocalk')) {
+        document.location.href = "calck.html";
+    }
 }
